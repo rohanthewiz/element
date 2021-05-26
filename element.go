072e8b74492db5abfe7338fe7b4dc12b3094a464
rohanthewiz <rohanthewiz@gmail.com>
2021-05-26 03:19:44 -0500

    Use String Builder

--- element.go
+++ element.go
@@ -1,18 +1,24 @@
 package element
 
 import (
+	"fmt"
+	"log"
 	"strings"
 )
 
 type Element struct {
-	El string // just the base of the element e.g. form
-	Attr map[string]string
+	El     string // just the base of the element e.g. form
+	attr   map[string]string
 	Single bool
+	sb     *strings.Builder
 }
 
 // Create a new element
-func New(el string, attrs ...string ) (Element) {
-	return Element{El: strings.ToLower(el), Attr: stringlistToMap(attrs...)}
+func New(sb *strings.Builder, el string, attrs ...string) (ele Element) {
+	if sb == nil {
+		log.Println("Please supply a pointer to a string builder to element.New():", el)
+	}
+	return Element{sb: sb, El: strings.ToLower(el), attr: stringlistToMap(attrs...)}
 }
 
 func (e Element) IsSingleTag() bool {
@@ -26,30 +32,43 @@ func (e Element) IsSingleTag() bool {
 func (e Element) AddAttributes(attrs ...string) {
 	m := stringlistToMap(attrs...)
 	for k, v := range m {
-		e.Attr[k] = v
+		e.attr[k] = v
 	}
 }
 
-// Render with `inner` as the innerHTML
-func (e Element) R(inner ...string) (str string) {
-	str = "<" + e.El
-
-	for k, v := range e.Attr {
-		str += " " + k + "=" + `"` + v + `"`
+// Render the element with `inner` as the innerHTML
+func (e Element) R(inner ...string) (out string) {
+	if e.sb != nil {
+		e.W(inner...)
+		out = e.sb.String()
 	}
+	return
+}
 
-	if !e.IsSingleTag() {
-		str += ">"
-		for _, r := range inner {
-			str += r
+// Write the element to the internal string builder but don't render a string
+func (e Element) W(inner ...string) {
+	if e.sb != nil {
+		e.sb.WriteString("<" + e.El)
+
+		for k, v := range e.attr {
+			e.sb.WriteString(" " + k + "=" + `"` + v + `"`)
 		}
-		str += "</" + e.El
+
+		if !e.IsSingleTag() {
+			e.sb.WriteString(">")
+			for _, r := range inner {
+				e.sb.WriteString(r)
+			}
+			e.sb.WriteString("</" + e.El)
+		}
+		e.sb.WriteString(">")
 	}
-	str += ">"
 	return
 }
 
 // Render element only if condition true
+// Note that it is better to do conditionals in an anonymous function
+// therefore we are deprecating this function. This function is deprecated.
 func (e Element) RIf(condition bool, inner ...string) (str string) {
 	if !condition {
 		return
@@ -57,37 +76,64 @@ func (e Element) RIf(condition bool, inner ...string) (str string) {
 	return e.R(inner...)
 }
 
-// Render each item in the slice wrapped in the Element el
-// with everything nested within the parent element
-// Attrs is a key, value list. A value may be marked as interpolatable with the iterated item with `{{$1}}`
-// A value in the attrs list may be marked as interpolatable with the iterated item
-func (e Element) For(arr []string, ele string, attrs ...string) string {
-	// Find and save the index of the first interpolatable attr if any
-	j := 0  // 0 is safe since we would never interpolate a key
-	for i, a := range attrs {
-		if i % 2 == 1 && a == "{{$1}}" {  // an attribute value wants to be interpolated
-			j = i; break
-		}
+// Normally render each item in the slice wrapped in the Element el
+// with everything nested within the parent element e
+// Attrs is a key, value list.
+// We will do interpolation in a separate function.
+// Note that it is better to do conditionals in an anonymous function
+func (e Element) For(items []string, ele string, attrs ...string) string {
+	sb := &strings.Builder{}
+	for _, item := range items {
+		el := Element{sb: sb, El: ele}
+		el.attr = stringlistToMap(attrs...)
+		el.W(item)
 	}
 
-	out := ""
-	el := Element{ El: ele }
-	for _, item := range arr {
-		if j > 0 {
-			attrs[j] = item  // replace
-		}
-		el.Attr = stringlistToMap(attrs...)
-		if j == 0 {
-			out += el.R(item) // render the element wrapping item
-		} else {
-			out += el.R()  // we already used the item in an attribute, so no wrap
-		}
-	}
-	return e.R(out)
+	fmt.Println("**->", sb.String())
+	return e.R(sb.String())
 }
 
+// TODO: Make another version of this function that only does interpolation
+// // Normally render each item in the slice wrapped in the Element el
+// // with everything nested within the parent element e
+// // Attrs is a key, value list.
+// // We will do interpolation in a separate function.
+// // strike thru: A value may be marked as interpolatable
+// // strike thru: with the iterated item with this expression `{{$x}}`
+// // strike thru: -- in this case it makes sense to have only one item in `items`
+// // Note that it is better to do conditionals in an anonymous function
+// func (e Element) For(items []string, ele string, attrs ...string) string {
+// 	// Find and save the index of the first interpolatable attr if any
+// 	j := 0  // 0 is safe since we would never interpolate a key
+// 	for i, a := range attrs {
+// 		if i % 2 == 1 && a == "{{$x}}" {  // an attribute value wants to be interpolated
+// 			j = i; break
+// 		}
+// 	}
+//
+// 	sb := &strings.Builder{}
+// 	for _, item := range items {
+// 		el := Element{sb: sb, El: ele}
+// 		if j > 0 {
+// 			attrs[j] = item  // replace - the replaceable attribute with
+// 			break
+// 		}
+// 		el.attr = stringlistToMap(attrs...)
+//
+// 		if j == 0 {
+// 			el.W(item)
+// 		} else {
+// 			el.W()  // we already used the item in an attribute, so no wrap
+// 		}
+// 	}
+//
+// 	// fmt.Println("**->", sb.String())
+// 	return e.R(sb.String())
+// }
 
 // Render as in For, but only if condition true
+// Note that it is better to do conditionals in an anonymous function
+// therefore we are deprecating this function. This function is deprecated.
 func (e Element) ForIf(condition bool, arr []string, el string, extra ...string) (str string) {
 	if !condition {
 		return
--- element_test.go
+++ element_test.go
@@ -1,29 +1,37 @@
 package element
 
 import (
+	"strings"
 	"testing"
 )
+
 var testAnimals = []string{"cat", "mouse", "dog"}
 
 func TestRender(t *testing.T) {
-	str := New("span").R()
+	sb := &strings.Builder{}
+	str := New(sb, "span").R()
 	if str != `<span></span>` {
 		t.Error("Failed to render an empty span")
 	}
-	str = New("span").R("This is some inner text")
+
+	sb.Reset()
+	str = New(sb, "span").R("This is some inner text")
 	if str != `<span>This is some inner text</span>` {
 		t.Error("Failed to render a span with inner text")
 	}
-	str = New("span", "id", "special", "class", "normal").R("This is some inner text")
+
+	sb.Reset()
+	str = New(sb, "span", "id", "special", "class", "normal").R("This is some inner text")
 	expected := `<span id="special" class="normal">This is some inner text</span>`
-	if len(str) != len(expected) {  // Go's map order is random, so have to rely on length match
+	if len(str) != len(expected) { // Go's map order is random, so have to rely on length match
 		t.Error("Failed to render a span with attributes and inner text",
 			"\nExpected:", expected, "\nGot:", str)
 	}
 }
 
 func TestAddAttributes(t *testing.T) {
-	el := New("span")
+	sb := &strings.Builder{}
+	el := New(sb, "span")
 	el.AddAttributes("id", "my-span", "class", "special-span")
 	str := el.R()
 	expected := `<span id="my-span" class="special-span"></span>`
@@ -33,29 +41,36 @@ func TestAddAttributes(t *testing.T) {
 }
 
 func TestRenderIf(t *testing.T) {
-	str := New("span").RIf(false, "This is some inner text")
+	sb := &strings.Builder{}
+	str := New(sb, "span").RIf(false, "This is some inner text")
 	if str != "" {
 		t.Error("False condition to RIf should yield empty string")
 	}
-	str = New("span").RIf(true, "This is some inner text")
+
+	sb.Reset()
+	str = New(sb, "span").RIf(true, "This is some inner text")
 	if str != `<span>This is some inner text</span>` {
 		t.Error("True condition to RIf should yield the rendered element")
 	}
 }
 
 func TestRenderOddNumOfAttributes(t *testing.T) {
-	str := New("img", "src", "http://example.com/test.png", "enable").R()
+	sb := &strings.Builder{}
+	str := New(sb, "img", "src", "http://example.com/test.png", "enable").R()
 	expected := `<img src="http://example.com/test.png">`
 	if str != expected {
 		t.Error("With odd number of attributes, element should drop the last:",
-		"\nExpected:", expected, "\nGot:", str)
+			"\nExpected:", expected, "\nGot:", str)
 	}
 }
 
 func TestFor(t *testing.T) {
-	str := New("div").R(
-		New("ul", "class", "list").For(testAnimals, "li"), // build a list
+	sb := &strings.Builder{}
+
+	str := New(sb, "div").R(
+		New(&strings.Builder{}, "ul", "class", "list").For(testAnimals, "li"), // build a list
 	)
+
 	expect := `<div><ul class="list"><li>cat</li><li>mouse</li><li>dog</li></ul></div>`
 	if str != expect {
 		t.Error("Iteration failed. Expected:", expect, "\nGot:", str)
@@ -63,27 +78,30 @@ func TestFor(t *testing.T) {
 }
 
 // Todo - Single tag elements
-func TestForWithInterpolation(t *testing.T) {
-	str := New("div", "class", "form-group").For(testAnimals, "input", "class", "text-input", "value", "{{$1}}")
-	expect := `<div class="form-group"><input class="text-input" value="cat"><input class="text-input" value="mouse"><input class="text-input" value="dog"></div>`
-	if len(str) != len(expect) {  // map ordering is random so test lengths as a compromise
-		t.Error("Iteration failed. Expected similar to:", expect, "\nGot:", str)
-	}
-}
-
+// func TestForWithInterpolation(t *testing.T) {
+// 	sb := &strings.Builder{}
+// 	str := New(sb, "div", "class", "form-group").For(testAnimals, "input", "class", "text-input", "value", "{{$1}}")
+// 	expect := `<div class="form-group"><input class="text-input" value="cat"><input class="text-input" value="mouse"><input class="text-input" value="dog"></div>`
+// 	if len(str) != len(expect) {  // map ordering is random so test lengths as a compromise
+// 		t.Error("Iteration failed. Expected similar to:", expect, "\nGot:", str)
+// 	}
+// }
+//
 func TestForIf(t *testing.T) {
 	animals := []string{"cat", "mouse", "dog"}
+	sb := &strings.Builder{}
 
-	str := New("div").R(
-		New("ul", "class", "list").ForIf(false, animals, "li"),
+	str := New(sb, "div").R(
+		New(sb, "ul", "class", "list").ForIf(false, animals, "li"),
 	)
 	expect := `<div></div>`
 	if str != expect {
 		t.Error("Iteration failed. Expected:", expect, "\nGot:", str)
 	}
 
-	str = New("div").R(
-		New("ul", "class", "list").ForIf(true, animals, "li"),
+	sb.Reset()
+	str = New(sb, "div").R(
+		New(sb, "ul", "class", "list").ForIf(true, animals, "li"),
 	)
 	expect = `<div><ul class="list"><li>cat</li><li>mouse</li><li>dog</li></ul></div>`
 	if str != expect {
