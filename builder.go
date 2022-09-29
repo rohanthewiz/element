eb9d682ee7cfd8763a74a7e46dc109cda5adc0c3
Rohan Allison <rohan.allison@cbre.com>
2022-09-29 14:02:09 -0500

    Add builder object

---
+++ builder.go
@@ -0,0 +1,36 @@
+package element
+
+import (
+	"strings"
+)
+
+// Builder has everything we need for building and rendering HTML
+type Builder struct {
+	Ele  elementFunc      // function for building an element
+	Text textFunc         // function for rendering literal text
+	s    *strings.Builder // accumulates our output
+}
+
+// NewBuilder returns a builder for creating our HTML
+func NewBuilder() (b *Builder) {
+	b = &Builder{}
+	b.s = &strings.Builder{}
+	b.Ele = func(el string, p ...string) Element {
+		return New(b.s, el, p...)
+	}
+	b.Text = func(p ...string) int {
+		return Text(b.s, p...)
+	}
+	return
+}
+
+// String returns the accumulated string output
+func (b *Builder) String() string {
+	return b.s.String()
+}
+
+// elementFunc build an element
+type elementFunc func(el string, p ...string) Element
+
+// textFunc renders literal text
+type textFunc func(p ...string) int
