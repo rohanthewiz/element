216db1df3efec78edcf6ac0296c63fe8efce59e8
Rohan Allison <rohan.allison@cbre.com>
2025-03-06 20:00:27 -0600

    Add convenient RenderComponents function

--- component.go
+++ component.go
@@ -4,3 +4,11 @@ package element
 //
 //	It takes an element.Builder instance  but returns nothing  since  it writes directly to the builder
 type Component func(b *Builder, components ...Component)
+
+// RenderComponents is used to render a list of components
+func RenderComponents(b *Builder, comps ...Component) (x any) {
+	for _, comp := range comps {
+		comp(b)
+	}
+	return
+}
