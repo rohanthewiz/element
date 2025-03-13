75f523ae5d577dd236015dcfa3973650d23d3054
Rohan Allison <rohanthewiz_at_gmail.com>
2025-03-12 22:34:41 -0500

    Define Component as an Interface

--- component.go
+++ component.go
@@ -1,14 +1,21 @@
 package element
 
-// Component defines a function type for rendering a group of HTML elements
+// Component defines an interface for rendering a group of HTML elements
 //
 //	It takes an element.Builder instance  but returns nothing  since  it writes directly to the builder
-type Component func(b *Builder, components ...Component)
+/*type Component func(b *Builder, components ...Component)
+ */
 
-// RenderComponents is used to render a list of components
+// Component is some group of elements that can be rendered in a tree of elements
+type Component interface {
+	// Render writes to the builder,  but returns anything so it can be rendered in a tree of elements
+	Render(b *Builder) (x any)
+}
+
+// RenderComponents is a convenience function for rendering a list of components in a tree of elements
 func RenderComponents(b *Builder, comps ...Component) (x any) {
 	for _, comp := range comps {
-		comp(b)
+		comp.Render(b)
 	}
 	return
 }
