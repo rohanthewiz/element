fa6ca65c3967b0462900edbc198c5c060ab74a5c
Rohan Allison <rohanthewiz_at_gmail.com>
2025-04-18 14:29:44 -0500

    WIP Add table component

---
+++ components/helpers.go
@@ -0,0 +1,19 @@
+package components
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+// genRandId generates a random id for an element
+func genRandId() string {
+	bytes := make([]byte, 12)
+
+	if _, err := rand.Read(bytes); err != nil {
+		return hex.EncodeToString([]byte(fmt.Sprintf("%d", time.Now().UnixMilli())))
+	}
+
+	return hex.EncodeToString(bytes)[:12]
+}
